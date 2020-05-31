18693bc47ab9b80848e7a8c05656c3744aafeb18
Kunal Jaiswal <kunaljaiswal2406@gmail.com>
2020-05-31 15:49:07 +0530

    park  wheels initial commit

---
+++ src/kunal/CheckLogin.java
@@ -0,0 +1,19 @@
+package kunal;
+
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class CheckLogin extends UnicastRemoteObject implements LoginInterface {
+
+    public CheckLogin() throws Exception {
+    }
+
+    public boolean verify(String userId, String password) throws RemoteException {
+        if (userId.equals("abc") && password.equals("abc")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/kunal/LoginInterface.java
@@ -0,0 +1,10 @@
+package kunal;
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface LoginInterface extends Remote {
+
+    boolean verify(String userId, String password) throws RemoteException;
+}
